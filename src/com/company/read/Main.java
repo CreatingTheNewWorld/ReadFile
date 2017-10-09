441ca7ff5ee8055063cecdfdb166dbc6ead0984c
Vitalii <SVU_591@ukr.net>
2017-10-09 15:35:59 +0300

    create that read file and output info

--- src/com/company/read/Main.java
+++ src/com/company/read/Main.java
@@ -1,10 +1,67 @@
 package com.company.read;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
 
-        
+    static  String s = "will be okey soon!";
+    static String[][] m = new String[2][4];
+    static Scanner scanner;
+
+    public static void main(String[] args) throws IOException {
+
+        openFile();
+        readFile();
+        out();
 
     }
+
+    private static void out() {
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+
+                System.out.print(m[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    private static void readFile() {
+
+        while (scanner.hasNext()){
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[i].length; j++) {
+
+
+                    if (scanner.hasNext() == false) {
+                        m[i][j] = "0";
+                    } else {
+                        m[i][j] = scanner.next();
+                    }
+
+                }
+            }
+        }
+        scanner.close();
+
+    }
+
+
+    private static void openFile() throws FileNotFoundException {
+
+        scanner = new Scanner(new File("src//1.txt"));
+
+
+
+    }
+
+
 }
